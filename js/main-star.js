413538180e514c838689efd8aaf7ac9fec444566
ckdgns121 <chlrkd347@naver.com>
2024-02-02 12:04:48 +0900

    ch_js수정

--- js/main-star.js
+++ js/main-star.js
@@ -37,7 +37,7 @@ printRatingResult(ratingResult);
 executeRating(ratingStars, ratingResult);
 
 function printRatingResult(result, rating) {
-   // result.textContent = rating ? `You rated: ${rating} stars` : "";
+
 }
 
 function executeRating(stars, result) {
@@ -50,13 +50,13 @@ function executeRating(stars, result) {
          if (star.className.indexOf(starClassUnactive) !== -1) {
             printRatingResult(result, index + 1);
             for (let i = index; i >= 0; --i) stars[i].className = starClassActive;
-            // You can customize the notification message here
+            
             alert(`별점 ${index + 1} 점을 입력하셨습니다.`);
          } else {
             printRatingResult(result, index);
             for (let i = index; i < starsLength; ++i) stars[i].className = starClassUnactive;
-            // You can customize the notification message here
-            // alert(`You removed your rating!`);
+            
+            
          }
       };
    });
